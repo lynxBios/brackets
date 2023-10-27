c9bfe1c05ddc455bc1b16d52f31bdc22c0e3cead
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-10-27 23:11:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let bracketsMap = {};
+
+  bracketsConfig.forEach(([openingBracket, closingBracket]) => {
+    bracketsMap[closingBracket] = openingBracket;
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    let currentBracket = str[i];
+
+    if (bracketsMap.hasOwnProperty(currentBracket)) {
+      let openingBracket = bracketsMap[currentBracket];
+
+      if (openingBracket === currentBracket) {
+        if (stack.length === 0 || openingBracket !== stack[stack.length - 1]) {
+          stack.push(currentBracket);
+        } else {
+          stack.pop();
+        }
+      } else {
+        if (stack.length === 0 || openingBracket !== stack.pop()) {
+          stack.push(currentBracket);
+        }
+      }
+    } else {
+      stack.push(currentBracket);
+    }
+  }
+
+  return stack.length === 0;
 }
